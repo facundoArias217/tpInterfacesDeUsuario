ef63bc81b1e1f6141c7aaac86cbf6dbefb6e24de
facundoArias217 <facundoarias5000@gmail.com>
2025-10-04 18:19:11 -0300

    P

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import {BrowserRouter} from 'react-router-dom'
+import './estilos/estilos.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -1,51 +1,58 @@
-import {Container, Row, Col, Image} from "react-bootstrap";
-import {Boton} from "../components";
-import {Link} from "react-router-dom";
+import { Container, Row, Col, Image } from "react-bootstrap";
+import { Boton } from "../components";
+import { Link } from "react-router-dom";
 
 //Arrglar las columnas y filas y tamaño de imagen y texto
 
 const Inicio = () => {
     const propsBotonIrAlCarrito = {
-        variant:'success',
-        className:'w-100',
-        as:Link,
-        to:'/Carta',
-        style:{backgroundColor:'#b79f7d', color:'black', border:'none', fontWeight:'bold'}
+        variant: 'success',
+        className: 'w-100',
+        as: Link,
+        to: '/Carta',
+        style: { backgroundColor: '#b79f7d', color: 'black', border: 'none', fontWeight: 'bold' }
     };
-    
+
     return (
-        <Container className="my-4">
-            <Row className="justify-content-sm-center" style={{marginBottom:'40px'}}>
-                <Col xs={12} md={6} lg={6}>
-                    <Image src="/logo.png" alt="logo" roundedCircle style={{width:'100%', height:'100%'}} />
-                </Col>
-                <Col md={12} lg={6}>
-                    <section>
-                        <h3>Nuestra Historia</h3>
-                        <br />
-                        <p style={{textAlign:"justify", marginBottom:'50px'}}>
-                            Hace más de veinte años, la familia Bianchi, 
-                            de raíces italianas, abrió esta cafetería con 
-                            un propósito claro: compartir la tradición de 
-                            la hospitalidad italiana (ospitalità) y el arte del buen café.
-                            Desde entonces, seleccionamos las mejores materias 
-                            primas: granos de altura de Latinoamérica y los 
-                            combinamos con ingredientes mediterráneos como aceite de oliva 
-                            y dulces artesanales, logrando sabores únicos y auténticos.
-                            Con el paso del tiempo, nuestro espacio se convirtió 
-                            en un punto de encuentro donde cada cliente es recibido como en casa.
-                            Hoy seguimos siendo fieles a nuestras raíces: cada 
-                            taza es tradición, hospitalidad y un abrazo cálido al alma.
-                        </p>
-                    </section>
-                    <Row className="justify-content-sm-center">
-                        <Col md={12} lg={8}>
-                            <Boton {...propsBotonIrAlCarrito} texto='Ver la Carta' />
+        <div>
+            <h1 className="titulos">Luna & granos café</h1>
+            <section className="secciones">
+                <Container className="my-4">
+                    <Row >
+                        <Col md={12} lg={6}>
+                            <div className="img-container">
+                            <Image src="/logo.png" alt="logo" className="imgs-grandes mx-auto d-block" />
+                            </div>
+                        </Col>
+                        <Col md={12} lg={6}>
+                            <section>
+                                <h2 className="subtitulos">Nuestra Historia</h2>
+                                <br />
+                                <p className="parrafos">
+                                    Hace más de veinte años, la familia Bianchi,
+                                    de raíces italianas, abrió esta cafetería con
+                                    un propósito claro: compartir la tradición de
+                                    la hospitalidad italiana (ospitalità) y el arte del buen café.
+                                    Desde entonces, seleccionamos las mejores materias
+                                    primas: granos de altura de Latinoamérica y los
+                                    combinamos con ingredientes mediterráneos como aceite de oliva
+                                    y dulces artesanales, logrando sabores únicos y auténticos.
+                                    Con el paso del tiempo, nuestro espacio se convirtió
+                                    en un punto de encuentro donde cada cliente es recibido como en casa.
+                                    Hoy seguimos siendo fieles a nuestras raíces: cada
+                                    taza es tradición, hospitalidad y un abrazo cálido al alma.
+                                </p>
+                            </section>
+                            <Row className="justify-content-sm-center">
+                                <Col md={12} lg={8}>
+                                    <Boton {...propsBotonIrAlCarrito} texto='Ver la Carta' />
+                                </Col>
+                            </Row>
                         </Col>
                     </Row>
-                </Col>
-            </Row>
-        </Container>
+                </Container>
+            </section>
+        </div>
     );
 };
 
