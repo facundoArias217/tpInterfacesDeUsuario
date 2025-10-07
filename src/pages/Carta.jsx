fa2a545d145c86e7319b01236b822116bb2cfe09
AsamFernando <ferasam88@gmail.com>
2025-10-06 21:03:58 -0300

    agregado componente badge en producto que muestra en cada card la cantidad añadida al carrito y su estilo global en estilos.css

--- src/pages/Carta.jsx
+++ src/pages/Carta.jsx
@@ -8,11 +8,11 @@ import menu from '../platos/platos.json';
 //poner un hover a cada card
 //poder ver la cantidad de cada producto en la card si ya fue añadido
 
-const Carta = ({accionBoton}) => {
+const Carta = ({accionBoton, pedido}) => {
     return (
         <Container className="my-4" fluid>
             <Row className="justify-content-lg-center">
-                {menu.map(p => <Col key={p.id} className="d-flex justify-content-center customStyle" xs={12} md={4} sm={6} lg={4} xl={2}>{<Producto producto={p} textoBoton='Añadir al Carrito' accion={accionBoton} />}</Col>)}
+                {menu.map(p => <Col key={p.id} className="d-flex justify-content-center customStyle" xs={12} md={4} sm={6} lg={4} xl={2}>{<Producto producto={p} textoBoton='Añadir al Carrito' accion={accionBoton} pedido={pedido} />}</Col>)}
             </Row>
         </Container>
     )
